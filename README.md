e1f0e9f156968732187ac959a8d7575ab8720d28
Elisabetta Fino <elisabetta.fino@gmail.com>
2025-12-03 18:31:43 +0100

    add ordinamento

--- README.md
+++ README.md
@@ -32,4 +32,23 @@ Ultima temperatura: 20
 Temperature [2:5]: [22, 25, 28]
 Ogni due: [15, 22, 28, 27, 20]
 
+## Esercizio 1.3: Ordinamento e Ricerca
+File: ordinamento.py
+Obiettivo: Ordinare liste e verificare esistenza elementi.
+Consegna:
+Creare una lista prezzi contenente: [45.5, 12.0, 78.3, 23.1, 56.7]
+Creare una copia ordinata della lista (usando sorted())
+Trovare il prezzo minimo e massimo
+Verificare se 23.1 è nella lista
+Contare quanti prezzi sono maggiori di 50
+
+Output Atteso:
+Prezzi originali: [45.5, 12.0, 78.3, 23.1, 56.7]
+Prezzi ordinati: [12.0, 23.1, 45.5, 56.7, 78.3]
+Minimo: 12.0
+Massimo: 78.3
+23.1 presente: True
+Prezzi > 50: 2
+
+
 
--- slicing.py
+++ slicing.py
@@ -6,22 +6,22 @@ Obiettivo: Praticare l'accesso agli elementi e lo slicing.
 Lista_iniziale = ["15", "18", "22", "25", "28", "30", "27", "24", "20"] 
 
 # Ordinare i numeri automaticamente
-Lista_ordinata = sorted(Lista_iniziale) 
+# Lista_ordinata = sorted(Lista_iniziale) 
 
 
 # ------------ #
 
 # Stampare la prima temperatura
-print(Lista_ordinata[0]) 
+print(Lista_iniziale[0]) 
 
 # Stampare l'ultima temperatura
-print(Lista_ordinata[-1]) 
+print(Lista_iniziale[-1]) 
 
 # Stampare le temperature dalla posizione 2 alla 5 (esclusa)
-print(Lista_ordinata[1:4])
+print(Lista_iniziale[1:4])
 
 # Stampare tutte le temperature con step 2 (saltando una ogni due)
-print(Lista_ordinata[::2])
+print(Lista_iniziale[::2])
 
 # Stampare la lista ordinata
-print(Lista_ordinata)
+print(Lista_iniziale)
